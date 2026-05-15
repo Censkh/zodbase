e66b748f5e987b1553ea5d7e36af2bb915831e58
James Waterhouse <09jwater@gmail.com>
2026-05-15 20:26:15 +0700

    FIX: build

--- src/index.ts
+++ src/index.ts
@@ -111,6 +111,7 @@ export interface DatabaseEvents {
 export interface DatabaseOptions {
   adaptor: DatabaseAdaptor;
   events?: DatabaseEvents;
+  debug?: boolean;
 }
 
 interface BaseFieldModification<M> {
@@ -491,14 +492,42 @@ export class Database {
   }
 
   async syncTable<TTable extends Table>(table: TTable): Promise<void> {
-    await this.options.adaptor.createTable(table);
+    const start = Date.now();
+    const log = (stage: string, extra?: Record<string, unknown>) => {
+      if (!this.options.debug) {
+        return;
+      }
+      console.info(`[zodbase.syncTable] ${stage}`, { table: table.id, ...extra });
+    };
+    log("start");
 
+    log("createTable:start");
+    await this.options.adaptor.createTable(table);
+    log("createTable:done", {
+      elapsedMs: Date.now() - start,
+    });
+
+    log("diffTable:start");
     const diff = await this.diffTable(table);
+    log("diffTable:done", {
+      elapsedMs: Date.now() - start,
+      diffCount: diff.fields.length,
+    });
 
     if (diff.fields.length > 0) {
+      log("processDiff:start", {
+        diff: diff.fields.map((field) => ({ key: field.key, type: field.type })),
+      });
       await this.adaptor.processDiff(table, diff);
+      log("processDiff:done", {
+        elapsedMs: Date.now() - start,
+      });
       //this.options.adaptor.executeSql(sql);
     }
+
+    log("done", {
+      elapsedMs: Date.now() - start,
+    });
   }
 
   private get adaptor() {
